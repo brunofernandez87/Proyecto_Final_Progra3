418d66575eed16a742d8a98f1b9a23d19bc84dfb
brunofernandez87 <brunoemanuelfernandez1@gmail.com>
2025-12-10 16:27:13 -0300

    Mejoras a la clase juego

--- src/Juego.java
+++ src/Juego.java
@@ -6,51 +6,56 @@ public class Juego {
     protected List<Personaje> jugador1 = new ArrayList<>();
     protected List<Personaje> jugador2 = new ArrayList<>();
     public void Menu(){
-        int opcion;
-        do{
+        int opcion=0;
         Scanner scanner = new Scanner(System.in);
+        do{
         System.out.println("Bienvenido al menu:");
         System.out.println("1 jugar");
         System.out.println("2 ver partidas anteriores");
         System.out.println("3 como jugar");
         System.out.println("0 salir");
         System.out.println("Ingrese una opcion");
-        opcion = scanner.nextInt();
-        opciones(opcion);
+            if (scanner.hasNextInt()) {
+                opcion = scanner.nextInt();
+                scanner.nextLine();
+                opciones(opcion, scanner);
+            } else {
+                System.out.println("Por favor ingrese un numero valido.");
+                scanner.next();
+            }
         }while(opcion != 0);
+        scanner.close();
     }
-    public void opciones(int opcion){
-        Scanner scanner = new Scanner(System.in);
+    public void opciones(int opcion,Scanner scanner){
             if (opcion != 0){
                 switch (opcion){
                     case 1:
+                        jugador1.clear();
+                        jugador2.clear();
                         System.out.println("¿Desea crear los personajes usted?");
-                        String opcion2= scanner.next();
-                        if (opcion2.toLowerCase().equals("si")){
-                            crearPersonajes(1);
-                            crearPersonajes(2);
-                            System.out.println("Esto son los personajes del jugador 1");
-                            Recorrer_personajes(jugador1);
-                            System.out.println("Estos son los personajes del jugador 2");
-                            Recorrer_personajes(jugador2);
-                            Partida();
-                            break;
+                        String opcion2= scanner.nextLine();
+                        if (opcion2.equals("si")){
+                            crearPersonajes(1, scanner);
+                            crearPersonajes(2, scanner);
                         }
                         else{
                             crearPersonajesRandom(1);
                             crearPersonajesRandom(2);
-                            System.out.println("Esto son los personajes del jugador 1");
-                            Recorrer_personajes(jugador1);
-                            System.out.println("Estos son los personajes del jugador 2");
-                            Recorrer_personajes(jugador2);
-                            Partida();
-                            break;
                         }
+                        System.out.println("Esto son los personajes del jugador 1");
+                        Recorrer_personajes(jugador1);
+                        System.out.println("Estos son los personajes del jugador 2");
+                        Recorrer_personajes(jugador2);
+                        Partida();
+                        break;
                     case 2:
                         System.out.println("Cargando partidas anteriores");
                         break;
                     case 3:
                         System.out.println("Las reglas del juego son:");
+                        break;
+                    default:
+                        System.out.println("Opcion no valida intente nuevamente");
                         break;
                 }
             }
@@ -64,39 +69,40 @@ public class Juego {
     //tirar un excepcion en caso de que todo este al maximo
     //usar excepcions en caso de superar el limite de rango de edad,velocidad,etc..
     // Sistema de puntos para crear personajes o limite de 2 stats al maximo
-    private void crearPersonajes(int jugador){
-        Scanner scanner =  new Scanner(System.in);
-        for (int i=1; i<=3; i++){
+    private void crearPersonajes(int jugador, Scanner scanner){
+        int i=0;
+       while (i < 3){
+           System.out.println("Creando el personaje numero "+(i+1)+"para el jugador "+jugador);
             Personaje personaje=null;
+           try {
             System.out.println("creando el personaje"+i);
             System.out.println("Escriba su nombre");
-            String nombre= scanner.next();
+            String nombre= scanner.nextLine();
             System.out.println("Escriba su apodo");
-            String apodo=scanner.next();
+            String apodo=scanner.nextLine();
             System.out.println("Escriba su fecha de nacimiento");
-            String nacimiento= scanner.next();
+            String nacimiento= scanner.nextLine();
             System.out.println("Escriba su edad");
             System.out.println("Entre 0 y 300");
-            int edad= scanner.nextInt();
+            int edad= Integer.parseInt(scanner.nextLine());
             System.out.println("Escriba su velocidad");
             System.out.println("Min 1 y Maximo 10");
-            int velocidad = scanner.nextInt();
+            int velocidad = Integer.parseInt(scanner.nextLine());
             System.out.println("Escriba su destreza");
             System.out.println("Min 1 y Maximo 5");
-            int destreza = scanner.nextInt();
+            int destreza = Integer.parseInt(scanner.nextLine());
             System.out.println("Escriba su fuerza");
             System.out.println("Min 1 y Maximo 10");
-            int fuerza = scanner.nextInt();
+            int fuerza =Integer.parseInt(scanner.nextLine());
             System.out.println("Escriba su nivel");
             System.out.println("Min 1 y Maximo 10");
-            int nivel= scanner.nextInt();
+            int nivel= Integer.parseInt(scanner.nextLine());
             System.out.println("Escriba su armadura");
             System.out.println("Min 1 y Maximo 10");
-            int armadura= scanner.nextInt();
+            int armadura=Integer.parseInt(scanner.nextLine());
             System.out.println("Eliga una raza:");
             System.out.println("Humano, Elfo u Orco");
-            String raza=scanner.next();
-            try {
+            String raza=scanner.nextLine();
                 switch(raza.toLowerCase()) {
                     case "humano":
                         personaje = new Humano(
@@ -115,28 +121,30 @@ public class Juego {
                         break;
                     default:
                         System.out.println("No selecciono ninguna opcion");
-                        break;
+                        continue;
                 }
 
             }catch (Exception e){
                 System.out.println(e);
-                break;
             }
-            System.out.println("Este es su personaje");
-            System.out.println(personaje.toString());
-            System.out.println("Desea borrarlo y crearlo de nuevo?");
-            String opcion2= scanner.next();
-            if (opcion2.equals("si")){
-                i=i-1;
-            }
-            else{
-                if (jugador==1){
-                    jugador1.add(personaje);
+            if(personaje!=null){
+                System.out.println("Este es su personaje");
+                System.out.println(personaje.toString());
+                System.out.println("Desea borrarlo y crearlo de nuevo?");
+                String opcion2= scanner.nextLine();
+                if (opcion2.equalsIgnoreCase("no")){
+                    if (jugador==1){
+                        jugador1.add(personaje);
+                    }else {
+                        jugador2.add(personaje);
+                    }
+                    i++;
+                    System.out.println("Okey creando siguiente personaje");
                 }else {
-                    jugador2.add(personaje);
+                    System.out.println("Creando devuelta el personaje");
                 }
-                System.out.println("Okey creando siguiente personaje");
             }
+
         }
     }
     private  void crearPersonajesRandom(int jugador){
@@ -157,19 +165,19 @@ public class Juego {
                     case 1:
                         personaje=new Humano(nombre,apodo,
                                 nacimiento,edad,velocidad,destreza,fuerza,nivel,armadura);
-                        System.out.println("Creando Humano");
+                        System.out.println("Creando Humano para jugador "+jugador);
 
                         break;
                     case 2:
                         personaje=new Elfo(nombre,apodo,
                                 nacimiento,edad,velocidad,destreza,fuerza,nivel,armadura);
-                        System.out.println("Creando Elfo");
+                        System.out.println("Creando Elfo para jugador "+jugador);
 
                         break;
                     case 3:
                         personaje=new Orco(nombre,apodo,
                                 nacimiento,edad,velocidad,destreza,fuerza,nivel,armadura);
-                        System.out.println("Creando orco");
+                        System.out.println("Creando orco para jugador "+jugador);
 
                         break;
                     case 4:
@@ -177,11 +185,16 @@ public class Juego {
                         break;
 
                 }
-                if (jugador == 1){
-                    jugador1.add(personaje);
-                }else{
-                    jugador2.add(personaje);
+                if(personaje!=null){
+                    if (jugador == 1){
+                        jugador1.add(personaje);
+                    }else{
+                        jugador2.add(personaje);
+                    }
+                }else {
+                    i--;
                 }
+
             }catch (Exception e){
                 System.out.println(e);
                 i=i-1;
@@ -198,8 +211,8 @@ public class Juego {
             int posicion2 = (int) (Math.random() * tamaño2);
             Personaje personaje1 = jugador1.get(posicion1);
             Personaje personaje2 = jugador2.get(posicion2);
-            System.out.println("Luchador del jugador 1 es "+personaje1.getNombre()+""+personaje1.getApodo()
-                    +"luchador del jugador 2 es "+personaje2.getNombre()+""+personaje2.getApodo());
+            System.out.println("Luchador del jugador 1 es "+personaje1.getNombre()+"'"+personaje1.getApodo()+" vs "
+                    +"luchador del jugador 2  "+personaje2.getNombre()+"'"+personaje2.getApodo());
             do {
                 if (personaje1.getSalud()>0) {
                     personaje1.atacar(personaje2);
@@ -225,9 +238,9 @@ public class Juego {
         }
     }
     public void Recorrer_personajes(List<Personaje>jugador){
-        for (int i=1;i<=3;i++){
-            Personaje personaje=jugador.get(i-1);
-            System.out.println("Este es el personaje numero "+i);
+        for (int i=0;i< jugador.size();i++){
+            Personaje personaje=jugador.get(i);
+            System.out.println("Este es el personaje numero "+ (i+1));
             System.out.println(personaje.toString());
         }
     }
